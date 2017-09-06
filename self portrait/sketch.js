0a02f91b2dd9ab0929fa5f2fbbabe7cd8f65c082
cristianpineda97 <31675550+cristianpineda97@users.noreply.github.com>
2017-09-05 20:35:18 -0400

    initial commit

---
+++ self portrait/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(648, 400);
+}
+function draw() {
+    background("black");
+    
+    fill("green");
+    rect(100, 100, 200, 250);
+    
+    fill("blue")
+    ellipse(150, 200, 50, 100);
+    ellipse(250, 200, 50, 80);
+
+    fill("white")
+    ellipse(150, 200, 50, 30);
+    ellipse(250, 200, 50, 30);
+    
+    line( )
+    
+}
