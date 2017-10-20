cf54b297a61537130c38be257ffd3995142579f0
cristianpineda97 <31675550+cristianpineda97@users.noreply.github.com>
2017-10-19 21:14:03 -0400

    midterm wave

---
+++ midterm/sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+	createCanvas(640, 640);
+	background("white");
+}
+    function draw() {
+        
+//        rect (260, 186, 120, 30);
+//        ellipse (320, 200, 50);    
+        
+        var offset = 250;
+        triangle(170, 75, 300, 165, 425, 75);
+        quad(170, 70, 300, 10, 425, 83, 10, 06);
+        fill("yellow");
+        
+}
