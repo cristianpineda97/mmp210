36d68badfc09b635578f76b5ff37c614b62f9d53
cristianpineda97 <31675550+cristianpineda97@users.noreply.github.com>
2017-10-24 21:26:49 -0400

    new new

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,15 +1,27 @@
 function setup() {
-	createCanvas(640, 640);
-	background("white");
+	createCanvas(1000, 550);
+	background("black");
 }
-    function draw() {
-        
-//        rect (260, 186, 120, 30);
-//        ellipse (320, 200, 50);    
+    function draw() {  
         
         var offset = 250;
-        triangle(170, 75, 300, 165, 425, 75);
-        quad(170, 70, 300, 10, 425, 83, 10, 06);
+        triangle(170, 75, 300, 165, 420, 75);
+        triangle(210, 300, 300, 455, 380, 300);
+        triangle(210, 300, 300, 100, 380, 300);
         fill("yellow");
+        noStroke();
+        
+        var columnNum = 5;
+var rowNum = 3;
+var columnSize = width/columnNum;
+var rowSize = height/rowNum;
+for (var x = 0; x <= width; x += columnSize) {
+	for (var y = 0; y <= height; y += rowSize) {
+		/* draw window here */
+        
+        ellipse(x,y,50)
+	}
         
 }
+        
+    }
