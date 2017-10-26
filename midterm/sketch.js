10441c793a482a4d5da4bba41e6d99e5ba168ad8
Cristian Pineda <pinedaboy97@gmail.com>
2017-10-26 17:32:33 -0400

    midterm almost complete

--- midterm/sketch.js
+++ midterm/sketch.js
@@ -1,36 +1,55 @@
 function setup() {
 	createCanvas(640, 640);
-	background ("black");
-	noStroke();
 }
 
-function mouseClicked() {
-//	design();
+function design() {
+        
+    var columns = 10;
+	var rows = 10;
+	var w = width/columns;
+	var h = height/rows;
+	let randomShape = floor(random(0,3));
     
-       var space = 5;        
-    var column = width/10;
-    var row = height/10;
-    var r = random(0,255);
-    var g = random(0,255);
-    var b = random(0,255);
+    var r = random(0, 255);
+    var g = random(0, 255);
+    var b = random(0, 255);
     
     background(r,g,b);
-    
-	    for (var x = 25; x <= width; x += column) {
-        for (var y = 30; y <= height; y += row) {
-            
-            r = random(200,255);
-            g = random(230,255);
-            b = random(230,255);
-            
-            //ellipse
-            stroke("orange");
-            strokeWeight(1);
-            fill("orange");
-            ellipse(x, y-4 , 40, 40);
 
+    
+	for (var x = 10; x < width; x += w) {
+		for (var y = 10; y < height; y += h) {
+            
+            
+            
+            if (randomShape == 0) {
+                console.log(randomShape);
+                background("green");
+                fill("red");
+                rect(x, y-4, 40 ,40);
+                
+            } else if (randomShape == 1) {
+                console.log(randomShape);
+                background("red");
+                fill("pink");
+                ellipse(x+20, y+20, 40 ,40);
+                
+            } else {
+                console.log(randomShape);
+                background("black");
+                fill("orange");
+                rect(x, y-4, 40 ,40);
+            }
+            
             
 		}
-    }
+	}
   
+}
+
+
+
+function mouseClicked() {
+	design();
+
 }
