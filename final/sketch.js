14489aaa01ed95b51c69e63b88b933cb70309529
cristianpineda97 <31675550+cristianpineda97@users.noreply.github.com>
2017-11-16 21:21:11 -0500

    final wave

--- final/sketch.js
+++ final/sketch.js
@@ -1,37 +1,53 @@
+var button;
 var images = [];
+var letters = [];
+var apple, bubblegum,cat, dominoes;
+
+function preload() {
+    apple = loadImage("img/apple.png");
+    images.push(apple);
+    bubblegum = loadImage("img/bubblegum.png");
+    images.push(bubblegum);
+    cat = loadImage("img/cat.png");
+    images.push(cat);
+    dominoes = loadImage("img/dominoes.png");
+    images.push(dominoes);
+}
+
 function setup() {
     createCanvas(648, 400);
-    background("red");
-    var apple = loadImage("apple.png");
-    images.push();
-    var bubble = loadImage("bubblegum.png");
-    images.push();
-    var cat = loadImage("cat.png");
-    images.push();
-    var dominoes = loadImage("dominoes.png");
-    images.push();
-    
-}
-
-function draw() {
-
-}
-
-function mousePressed() {
-    background("red");
-    var letters = [
+    background("white");
+      button = createButton('PRESS');
+      button.position(width/2, 500);
+      button.mousePressed(changeImage);
+    console.log(images);
+     letters = [
         'a',
         'b',
         'c',
         'd'
     ];
+//    image(apple, width/2, height/2);
+//    image(bubblegum, width/2, height/2);
+//    image(cat, width/2, height/2)
+//    image(dominoes, width/2, height/2)
+//    console.log(letters[0]);
+}
+
+function changeImage() {
     
-    var randomLetter = floor(random(0, letters.length));
-    var letter = letters[randomLetter];
-    
-    textWidth(width);
-    textAlign(CENTER);
-    var myName = "cristian";
-    fill("black");
-    text(letter, width/2, height/2);
+}
+
+function mousePressed() {
+//    background("white");
+//   
+//    
+//    var randomLetter = floor(random(0, letters.length));
+//    var letter = letters[randomLetter];
+//    
+//    textWidth(width);
+//    textAlign(CENTER);
+//    var myName = "cristian";
+//    fill("black");
+//    text(letter, width/2, height/2);
 }
