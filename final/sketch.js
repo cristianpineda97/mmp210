e23cb4656c8505c133d3014d60316c7668ab0b7e
cristianpineda97 <31675550+cristianpineda97@users.noreply.github.com>
2017-11-28 20:43:15 -0500

    its lit

--- final/sketch.js
+++ final/sketch.js
@@ -1,7 +1,7 @@
 var button;
 var images = [];
 var letters = [];
-var apple, bubblegum, cat, dominoes;
+var apple, bubblegum, cat, dominoes, elevator, floppydisk, gorilla, hearts, iguana, jackolantern, kite, lens, mansion, nasa, omelette, poker, questionmark, ribbon, superman, television, umbrella, videocamera, waterdrop, xanax, youtube, zombie;
 
 function preload() {
     apple = loadImage("img/apple.png");
@@ -14,6 +14,48 @@ function preload() {
     images.push(dominoes);
     elevator = loadImage("img/elevator.png");
     images.push(elevator)
+    floppydisk = loadImage("img/floppydisk.png");
+    images.push(floppydisk)
+    gorilla = loadImage("img/gorilla.png");
+    images.push(gorilla)
+    hearts = loadImage("img/hearts.png");
+    images.push(hearts)
+    iguana = loadImage("img/iguana.png");
+    images.push(iguana)
+    jackolantern = loadImage("img/jackolantern.png");
+    images.push(jackolantern)
+    kite = loadImage("img/kite.png");
+    images.push(kite)
+    lens = loadImage("img/lens.png");
+    images.push(lens)
+    mansion = loadImage("img/mansion.png");
+    images.push(mansion)
+    nasa = loadImage("img/nasa.png");
+    images.push(nasa) 
+    omelette = loadImage("img/omelette.png");
+    images.push(omelette) 
+    poker = loadImage("img/poker.png");
+    images.push(poker) 
+    questionmark = loadImage("img/questionmark.png");
+    images.push(questionmark) 
+    ribbon = loadImage("img/ribbon.png");
+    images.push(ribbon) 
+    superman = loadImage("img/superman.png");
+    images.push(superman) 
+    television = loadImage("img/television.png");
+    images.push(television)
+    umbrella = loadImage("img/umbrella.png");
+    images.push(umbrella)
+    videocamera = loadImage("img/videocamera.png");
+    images.push(videocamera)
+    waterdrop = loadImage("img/waterdrop.png");
+    images.push(waterdrop)
+    xanax = loadImage("img/xanax.png");
+    images.push(xanax)
+    youtube = loadImage("img/youtube.png");
+    images.push(youtube)
+    xzombie = loadImage("img/zombie.png");
+    images.push(zombie)
 }
 
 function setup() {
